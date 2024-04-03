f29cb37bfd9bcbeb4bd5dbe57afd67750e8271fa
bahati-hakizimana <hakizimanabahati2019@gmail.com>
2024-04-03 07:13:24 -0700

    calculation

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Well come to pointers Phase")
+
+	// var ptr *int
+
+	// fmt.Println("The value of this is", ptr)
+
+
+	myNumber := 23
+
+	var ptr = &myNumber
+
+
+	fmt.Println("the actual value of pointer is", ptr)
+	fmt.Println("the actual value of pointer is", *ptr)
+
+	*ptr = *ptr * 4
+
+	fmt.Println("The new value of pointer is :", myNumber)
+
+}
